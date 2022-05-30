12e3e7a658cba2e9a299a5efa3095aa2c5db297d
Joel Giannelli <jgiannelli13@gmail.com>
2022-05-29 22:28:28 -0700

    Messing with spring core

--- src/main/java/com/example/movierec/MovierecApplication.java
+++ src/main/java/com/example/movierec/MovierecApplication.java
@@ -1,17 +1,24 @@
 package com.example.movierec;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 @SpringBootApplication
 public class MovierecApplication {
 
+
+	//Bean definitions
+
 	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-		RecommenderImplementation recommender = new RecommenderImplementation();
-		String[] result = recommender.recommendMovie("Test");
 
-		SpringApplication.run(MovierecApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/example/movierec/User.java
@@ -0,0 +1,18 @@
+package com.example.movierec;
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void displayInfo() {
+        System.out.println("The user is called: " + this.name);
+    }
+
+}
