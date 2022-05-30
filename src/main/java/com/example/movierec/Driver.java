ea74e23ac1fff883619286dff2d8fb7b3d1e1f27
Joel Giannelli <jgiannelli13@gmail.com>
2022-05-29 22:30:15 -0700

    file name changes

--- src/main/java/com/example/movierec/Driver.java
+++ src/main/java/com/example/movierec/Driver.java
@@ -10,7 +10,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 @SpringBootApplication
-public class MovierecApplication {
+public class Driver {
 
 
 	//Bean definitions
