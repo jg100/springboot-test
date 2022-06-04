69b306ee2d4e5b62b340193eaaddd9baa61db1c3
Joel Giannelli <jgiannelli13@gmail.com>
2022-06-04 16:47:24 -0700

    Testing driver class after annotation config

--- src/main/java/annotation/AnnotationTestDriver.java
+++ src/main/java/annotation/AnnotationTestDriver.java
@@ -1,6 +1,17 @@
 package annotation;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AnnotationTestDriver {
 
+    public static void main(String[] args) {
+        //Same as importing the xml but instead java based
+        ApplicationContext context = new AnnotationConfigApplicationContext(UserConfig.class);
+        User user = (User) context.getBean(User.class);
 
+        System.out.println("**** Output *****");
+        System.out.println(user.getName());
+
+    }
 }
--- src/main/java/annotation/UserConfig.java
+++ src/main/java/annotation/UserConfig.java
@@ -1,13 +1,20 @@
 package annotation;
-import org.graalvm.compiler.lir.LIRInstruction;
 import org.springframework.context.annotation.*;
 
 @Configuration
 public class UserConfig {
-
+    /**
+     * The method and annotations are equivalent to the
+     * following xml in the xml config file
+     *
+     * <beans>
+     *     <bean id="helloWorld" class="annotation"/>
+     *     <beans/>
+     * @return
+     */
     @Bean
-    public UserConfig() {
-        return new UserConfig();
+    public User user() {
+        return new User();
     }
 
 }
