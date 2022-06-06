4c7ae874f74ca74a87c0bafd257dfddad2c58707
Joel Giannelli <jgiannelli13@gmail.com>
2022-06-05 21:09:53 -0700

    backup

--- src/main/java/annotation/TextEditorConfig.java
+++ src/main/java/annotation/TextEditorConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class TextEditorConfig {
     @Bean
     public TextEditor texteditor() {
+        //Error
         return new TextEditor(spellchecker());
     }
 
